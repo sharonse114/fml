34882f1e99a9902280da6e1f63080f47e5f687d4
Sharon <sharon@Kaseys-MacBook-Air.local>
2018-09-20 20:33:37 -0700

    First commit

---
+++ server/server.js
@@ -0,0 +1,19 @@
+//server/server.js
+var express = require('express');
+var router = require('./routes/routes.js')
+var path = require('path');
+var bodyParser = require('body-parser');
+var app = express();
+var mongoose = require('mongoose');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../client'));
+app.use(express.static(path.join(__dirname, '../client')));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: false}));
+
+mongoose.connect('mongodb://sharonse:abc123@ds249942.mlab.com:49942/expenses',{ useNewUrlParser: true });
+
+app.use('/', router);
+
+module.exports=app;
